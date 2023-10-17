d5b1115a785a75486550bf7f6a1011e8bbf8c805
KrisGromova <levkindom@gmail.com>
2023-10-17 17:54:07 +0300

    HW_2.1.3

---
+++ task_score.js
@@ -0,0 +1,14 @@
+let introduction = 300;
+let git = 200;
+let js = 100;
+let total = introduction + git + js;
+console.log(total / 4);
+
+let floatNumber = total / 4 
+console.log(`There is avarage score is equal to ${floatNumber}.`)
+
+let newName = "Kris"
+let surName = "Gromova"
+let fullName = newName + " " + surName
+let studName = fullName
+console.log(`The average value is ${studName}.`)
