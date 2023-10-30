4647b1fb0be691bc06d9a2e417e3902b2f4cd82b
KrisGromova <levkindom@gmail.com>
2023-10-31 01:40:43 +0300

    hw_2.4.1

---
+++ hw_2.4.1.js
@@ -0,0 +1,28 @@
+let yearlyExpences = [800, 450, 2000, 45550, 5000, 999, 3400, 4000, 5500, 1500]
+function calculateExpences(expences) {
+    let totalExpences = 0;
+    for (let i = 0; i < expences.length; i++) {
+      if (expences[i] > 1000) {
+        totalExpences += expences[i];
+      }
+    }
+    return totalExpences;
+  }
+  
+  let total = calculateExpences(yearlyExpences);
+  console.log("Сумма затрат выше 1000: " + total);
+
+  let expencesExamples = [
+    { yearlyExpences: [2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390] },
+    { yearlyExpences: [500, 1000, 90000, 80, 980, 9000, 10, 400, 3000, 250, 45000, 1200] },
+    { yearlyExpences: [20, 200, 8900, 3300, 2300, 1009, 1200, 2000, 670, 900, 900, 7000] },
+   ];
+  
+ function testCalculateExpences() {
+    expencesExamples.forEach((example) => {
+      let total = calculateExpences(example.yearlyExpences);
+      console.log("Сумма затрат выше 1000: " + total);
+    });
+  }
+  
+  testCalculateExpences();
