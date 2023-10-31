de8acded22482b65fe7f3bdc55d4b61ef234d854
KrisGromova <levkindom@gmail.com>
2023-10-31 12:29:52 +0300

    hw_2.4.1

--- hw_2.4.1.js
+++ hw_2.4.1.js
@@ -1,6 +1,4 @@
-let yearlyExpences = [800, 450, 2000, 45550, 5000, 999, 3400, 4000, 5500, 1500, 789, 7000]
-const monthes = ["январь", "февраль", "март", "апрель", "май", "июнь", "июль", "август", "сентябрь", "октябрь", "ноябрь", "декабрь"]
-
+let yearlyExpences = [800, 450, 2000, 45550, 5000, 999, 3400, 4000, 5500, 1500, 7564, 2350]
 function calculateExpences(expences) {
     let totalExpences = 0;
     for (let i = 0; i < expences.length; i++) {
@@ -13,21 +11,6 @@ function calculateExpences(expences) {
   
   let total = calculateExpences(yearlyExpences);
   console.log("Сумма затрат выше 1000: " + total);
-
-  //выводим месяцы
-
-  function getMonth(expences) {
-      let monthNames = [];
-      for (let i = 0; i < expences.length; i++) {
-      if (expences[i] <= 1000) {
-         monthNames.push(monthes[i]);
-             } 
-    }
-      return monthNames;
-  }
-  
-  let monthNames = getMonth(yearlyExpences);
-  console.log("Месяцы с затратами меньше или равными 1000: " + monthNames.join(", "));
 
   let expencesExamples = [
     { yearlyExpences: [2500, 6000, 7000, 6000, 3, 9, 700, 8000, 655, 5700, 340, 5390] },
@@ -43,18 +26,3 @@ function calculateExpences(expences) {
   }
   
   testCalculateExpences();
-
-  function getMonth(expences) {
-    let monthNames = [];
-      for (let i = 0; i < expences.length; i++) {
-      if (expences[i] <= 1000) {
-        let currentDate = new Date(i + 1);
-        monthNames.push(currentDate.toLocaleString('default', { month: 'long' }));
-      }
-    }
-  
-    return monthNames;
-  }
-  
-  let monthNamesWithLowExpences = getMonth(yearlyExpences);
-  console.log("Месяцы с затратами меньше или равными 1000: " + monthNamesWithLowExpences.join(", "));
