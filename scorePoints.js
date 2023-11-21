79ac2c31d043f08dc85f70d88e55fa527f783b93
KrisGromova <levkindom@gmail.com>
2023-11-21 15:10:26 +0300

    change hw_3.2.2

--- scorePoints.js
+++ scorePoints.js
@@ -1,51 +1,77 @@
 const students = [
   [
-    { "name": "Ivan", "scorePoints": 35, "date": "2022-10-11" },
-    { "name": "Maria", "scorePoints": 5, "date": "2022-10-10" },
-    { "name": "Olga", "scorePoints": 0, "date": "" },
-    { "name": "Stepan", "scorePoints": 35, "date": "2022-10-12" },
-    { "name": "Oleg", "scorePoints": 9, "date": "2022-10-01" },
-    { "name": "Zanna", "scorePoints": 15, "date": "2022-10-11" }
+    { name: "Ivan", scorePoints: 35, date: "2022-10-11" },
+    { name: "Maria", scorePoints: 5, date: "2022-10-10" },
+    { name: "Olga", scorePoints: 0, date: "" },
+    { name: "Stepan", scorePoints: 35, date: "2022-10-12" },
+    { name: "Oleg", scorePoints: 9, date: "2022-10-01" },
+    { name: "Zanna", scorePoints: 15, date: "2022-10-11" },
   ],
   [
-    { "name": "Margo", "scorePoints": 0, "date": "2022-10-11" },
-    { "name": "Natalia", "scorePoints": 25, "date": "2022-10-10" },
-    { "name": "Marina", "scorePoints": 25, "date": "2022-10-01" },
-    { "name": "Dmitry", "scorePoints": 25, "date": "2022-10-12" },
-    { "name": "Denis", "scorePoints": 0, "date": "2022-10-02" },
-    { "name": "Vadimyr", "scorePoints": 1, "date": "2022-10-11" }
+    { name: "Margo", scorePoints: 0, date: "2022-10-11" },
+    { name: "Natalia", scorePoints: 25, date: "2022-10-10" },
+    { name: "Marina", scorePoints: 25, date: "2022-10-01" },
+    { name: "Dmitry", scorePoints: 25, date: "2022-10-12" },
+    { name: "Denis", scorePoints: 0, date: "2022-10-02" },
+    { name: "Vadimyr", scorePoints: 1, date: "2022-10-11" },
   ],
   [
-    { "name": "Irina", "scorePoints": 0, "date": "2022-10-11" },
-    { "name": "Vasily", "scorePoints": 0, "date": "2022-10-10" },
-    { "name": "David", "scorePoints": 0, "date": "2022-10-11" },
-    { "name": "Kristina", "scorePoints": 0, "date": "2022-10-12" },
-    { "name": "Varvara", "scorePoints": 0, "date": "2022-10-01" },
-    { "name": "Tanya", "scorePoints": 0, "date": "2022-10-11" }
-  ]
-]
+    { name: "Irina", scorePoints: 0, date: "2022-10-11" },
+    { name: "Vasily", scorePoints: 0, date: "2022-10-10" },
+    { name: "David", scorePoints: 0, date: "2022-10-11" },
+    { name: "Kristina", scorePoints: 0, date: "2022-10-12" },
+    { name: "Varvara", scorePoints: 0, date: "2022-10-01" },
+    { name: "Tanya", scorePoints: 0, date: "2022-10-11" },
+  ],
+];
 function scorePointsStud(arr) {
+  // создаем массив, куда будем заносить значения полей scorePoint
+
   let scores = [];
+
+  // циклом перебираем поля scorePoints и заносим их в массив scores
 
   for (let el in arr) {
     scores.push(arr[el].scorePoints);
   }
 
+  // находим максимальное значение из всех в scores
 
   let maxScore = Math.max(...scores);
 
- 
+  // создаем объект, куда будем заносить значения формата key - значение students.name, value - значение students.date
+
+  let dateObj = {};
+
+  // циклом перебираем students
+
   for (let el in arr) {
+    // если значение scorePoints является максимальным, то заносим в dateObj имя студента и дату в формате количества секунд
+
     if (arr[el].scorePoints === maxScore) {
-      console.log(
-        `Congratulations! ${arr[el].name}, you have earned the maximum points at ${arr[el].date}, its ${maxScore} `
-      );
-       }
-         }
-         return maxScore
+      dateObj[arr[el].name] = new Date(arr[el].date).getTime();
+    }
+  }
+
+  // присваиваем переменной значение имени студента, у которого наименьшее количество в поле value в dateObj.
+
+  let minDateKeyName = Object.keys(dateObj).reduce((a, b) =>
+    dateObj[a] < dateObj[b] ? a : b
+  );
+
+  // выводим сообщение с именем студента, у которого наименьшее время выполнение задания и максимальный балл,
+  //  а также дату выполнения. дату получаем обратно из секунд value в dateObj
+
+  console.log(
+    `Congratulations! ${minDateKeyName}, you have earned the maximum ${maxScore} points at ${
+      new Date(Math.min(...Object.values(dateObj))).toISOString().split("T")[0]
+    }`
+  );
+
+  // возвращаем значение maxScore , нужен для тестов
+
+  return maxScore;
 }
 
-students.forEach((el)=> scorePointsStud(el))
-
+students.forEach((el) => scorePointsStud(el));
 module.exports = scorePointsStud;
- 
